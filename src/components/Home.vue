1eea97eee21acc08e350e1900154c8e658273cf7
Vsoapmac <17724147277@163.com>
2020-08-11 11:38:23 +0800

    post

---
+++ src/components/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <body>
+  <h1>This is Home</h1>
+    <div>
+      <form>
+        <input type="text" placeholder="writeSomething"/>
+        <input type="submit" @click="submit()"/>
+      </form>
+      <h3>return {{success}}</h3>
+    </div>
+  </body>
+</template>
+
+<script>
+  export default ({
+    data(){
+      return{
+        success : 1,
+      }
+    },
+    methods: {
+      submit() {
+        this.$axios({
+          method: 'post',
+          url:'/connectVue'
+        })
+          .then(response => {
+            var success = response.data.success;
+            this.success = success;
+          })
+          .catch(function (error) {
+            console.log(error);
+          })
+      }
+    }
+  })
+</script>
+
+<style scoped>
+
+</style>
